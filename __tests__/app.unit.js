c3f8377d7ee49616a769b37fe5840496046839ac
tluolamo <tluolamo@gmail.com>
2020-06-20 11:00:51 -0700

    Add some basic tests

--- __tests__/app.unit.js
+++ __tests__/app.unit.js
@@ -8,3 +8,46 @@ describe('Test Homepage', () => {
     expect(response.statusCode).toBe(200)
   })
 })
+
+describe('Test /api/shorturl/new', () => {
+  test('It should return shortened URL for good URLs', async () => {
+    const goodUrls = [
+      'https://www.google.com/'
+    ]
+
+    for (const url of goodUrls) {
+      const response = await request(app).post('/api/shorturl/new', url)
+      expect(response.statusCode).toBe(200)
+      expect(response.body.original_url).toBe('www.google.com')
+      expect(typeof response.body.short_url).toBe('number')
+    }
+  })
+
+  test('It should return error for invalid urls', async () => {
+    const badUrls = [
+      'https://www.googledasfdfasfasdfdfdafdafddf.com',
+      'htts://www.google.com/',
+      'https://www.google.com/ /'
+    ]
+
+    for (const url of badUrls) {
+      const response = await request(app).post('/api/shorturl/new', url)
+      expect(response.body).toBe('{"error":"invalid URL"}')
+    }
+  })
+})
+
+describe('Test /api/shorturl/::number::', () => {
+  test('It should respond to the GET method and return 301 redirect', async () => {
+    const response = await request(app).get('/')
+    expect(response.statusCode).toBe(301)
+  })
+})
+
+// /api/shorturl/3
+
+/*
+1. I can POST a URL to `[project_url]/api/shorturl/new` and I will receive a shortened URL in the JSON response. Example : `{"original_url":"www.google.com","short_url":1}`
+2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.example.com(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`. *HINT*: to be sure that the submitted url points to a valid site you can use the function `dns.lookup(host, cb)` from the `dns` core module.
+3. When I visit the shortened URL, it will redirect me to my original link.
+*/
