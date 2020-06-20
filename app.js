6426307b0bc386e1ae33e3e2ca88fa75ace43060
tluolamo <tluolamo@gmail.com>
2020-06-20 15:48:02 -0700

    cleaned up some of the code and prevented unnecessary creation of new seq ids

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const ShortURL = require('./shortURL')
 const app = express()
 
 /** this project needs a db !! **/
-mongoose.set('debug', true)
+// mongoose.set('debug', true)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
@@ -34,58 +34,68 @@ app.get('/', async (req, res) => {
 app.post('/api/shorturl/new', async (req, res) => {
   // console.log(req.body)
   const urlStr = req.body.url
-  let myURL
-  let data = { error: 'invalid URL' }
+  const query = { url: urlStr }
+
+  let myURL, shortURLRec, data
 
   try {
     // parse the url, this should throw type error for invalid url
     myURL = new url.URL(urlStr)
-    console.log(myURL)
+    // console.log(myURL)
   } catch (err) {
     console.log(err)
+    data = { error: 'invalid URL' }
   }
 
   if (myURL) {
+    // we have valid url
     try {
-      // we have valid url, lets check the db for existing entry, returns existing or inserts new
-      const entry = await ShortURL.findOneAndUpdate(
-        { url: urlStr },
-        {
-          $setOnInsert: {
-            seq: await getNextSequenceValue('shortURL'),
-            url: urlStr
-          }
-        },
-        {
-          new: true, // return new doc if one is upserted
-          upsert: true // insert the document if it does not exist
-        }
-      )
-
-      // set the return data set correctly
-      data = { original_url: myURL.host, short_url: entry.seq }
+      // first check if the record exists in the db, this is to prevent creating new sequence number every time we run this code.
+      shortURLRec = await ShortURL.findOne(query)
     } catch (err) {
       console.log(err)
       data.error = 'DB error'
     }
+
+    // entry doesn't exist in the db
+    if (!shortURLRec) {
+      // try find it once more incase race condition and if it doesn't exists insert it with new sequence number instead
+      try {
+        shortURLRec = await ShortURL.findOneAndUpdate(
+          query,
+          {
+            $setOnInsert: {
+              seq: await getNextSequenceValue('shortURL'),
+              url: urlStr
+            }
+          },
+          {
+            new: true, // return new doc if one is upserted
+            upsert: true // insert the document if it does not exist
+          }
+        )
+      } catch (err) {
+        console.log(err)
+        data.error = 'DB error'
+      }
+    }
+  }
+
+  // set the return data correctly if there were no errors
+  if (!data) {
+    data = { original_url: myURL.host, short_url: shortURLRec.seq }
   }
 
   res.json(data)
 })
-// {"_id":"shortURL","value":{"$numberLong":"0"}}
+
 // this is to keep track of auto incrment field
 const getNextSequenceValue = async (sequenceName) => {
-  console.log(Buffer.byteLength(sequenceName, 'utf8'))
   const sequenceDocument = await Counters.findByIdAndUpdate(
     sequenceName,
     { $inc: { seq_value: 1 } },
     { new: true, upsert: true })
   return sequenceDocument.seq_value
-}
-
-// find by url
-const findByURL = async (url) => {
-  const data = await ShortURL.find({ url })
 }
 
 module.exports = app
