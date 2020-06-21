48d89434a35c54ce05eb98279df496488f9a44d2
tluolamo <tluolamo@gmail.com>
2020-06-20 20:58:20 -0700

    Small cleanup

--- app.js
+++ app.js
@@ -3,10 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const url = require('url')
-// const validator = require('validator')
-// var mongo = require('mongodb')
 const mongoose = require('mongoose')
-
 const cors = require('cors')
 
 const Counters = require('./counters')
